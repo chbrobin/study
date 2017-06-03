72129d37c71951226c00ded9eee852a25a1f461c
chbrobin <chbrobin@gmail.com>
2017-06-03 23:09:43 +0800

    exception test code update'

--- src/main/java/java/Test.java
+++ src/main/java/java/Test.java
@@ -2,6 +2,12 @@ package java;
 
 /**
  * Created by Administrator on 2017/6/3 0003.
+ * 用不需查异常强化方法的约定 类似的有NullPointerException IndexOutOfBoundsException 等
+ * 可参考java.lang包下 Error & Exception 相关类 主要是RuntimeException
  */
 public class Test {
+
+    public static void main(String[] args) {
+        Test test = new Test(); // throw java.lang.SecurityException
+    }
 }
--- src/main/java/pers/chbrobin/study/jdk/exception/NestedExceptionTest.java
+++ src/main/java/pers/chbrobin/study/jdk/exception/NestedExceptionTest.java
@@ -1,7 +1,13 @@
 package pers.chbrobin.study.jdk.exception;
 
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2017/6/3 0003.
+ * 用链式异常来保存原始异常信息
+ * Throwable提供四种构造方法，其中有可实现链式异常
+ * 不要在finally块中抛出异常，JVM先执行finally块，然后再时行异常处理
+ * 如果finally中有异常抛出，就会将原先异常掩没，这个与JVM操作机制有关 先处理finally逻辑再处理异常逻辑
  */
 public class NestedExceptionTest extends Exception {
     public NestedExceptionTest(){
@@ -21,4 +27,22 @@ public class NestedExceptionTest extends Exception {
     }
 
 
+    private static void throwOldException() throws IOException {
+        throw new IOException("a forced Exception");
+    }
+
+    private static void throwNestedException() throws NestedExceptionTest {
+        try {
+            throwOldException();
+        } catch (IOException e) {
+            throw new NestedExceptionTest(e);
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            throwNestedException(); // 会同时抛出IOException和NestedExceptionTest
+        } catch (NestedExceptionTest e) {
+            e.printStackTrace();
+        }
+    }
 }
