94e35d347344e51e5eb429153596d92f1ea6eea5
chbrobin <chbrobin@gmail.com>
2017-06-04 23:06:42 +0800

    add test code

--- src/main/java/pers/chbrobin/study/jdk/ArrayTest.java
+++ src/main/java/pers/chbrobin/study/jdk/ArrayTest.java
@@ -1,7 +1,41 @@
 package pers.chbrobin.study.jdk;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/6/4 0004.
  */
 public class ArrayTest {
+    public static void main(String[] args) {
+        int[] i = new int[]{1,2,3};
+        System.out.println(i);//存值
+        char[] c = new char[]{'a','b','c'};
+        System.out.println(c);//存值
+
+        Integer[] ii = new Integer[]{1,2,3};
+        System.out.println(ii);//存引用
+        String[] cc = new String[]{"a","b","c"};
+        System.out.println(cc);//存引用
+
+        // 排序、反转
+        Integer[] si = new Integer[]{2,1,3};
+        Arrays.sort(si);
+        List<Integer> siList = (List<Integer>)Arrays.asList(si);
+        Collections.reverse(siList);
+        System.out.println(Arrays.toString(si));
+
+        int[] fi = new int[10];
+        Arrays.fill(fi,10);
+        System.out.println(Arrays.toString(fi));
+
+        System.out.println(Arrays.binarySearch(i,2)); // 1
+
+        int[] ci = new int[20];
+        System.arraycopy(i, 0, ci, 4, 3);
+        System.out.println(Arrays.toString(ci));
+
+        ci.equals(i);
+    }
 }
--- src/main/java/pers/chbrobin/study/thread/ThreadGroupTest.java
+++ src/main/java/pers/chbrobin/study/thread/ThreadGroupTest.java
@@ -2,6 +2,41 @@ package pers.chbrobin.study.thread;
 
 /**
  * Created by Administrator on 2017/6/4 0004.
+ * main's parent is system
+ * thread main group name is main
  */
 public class ThreadGroupTest {
+    public static void main(String[] args) {
+        printGroupInfo(Thread.currentThread());
+
+        Thread appThread = new Thread(new Runnable(){
+            public void run() {
+                for (int i=0;i<5;i++) {
+                    System.out.println("do loop " + i);
+                }
+            }
+        });
+        appThread.setName("appThread");
+        appThread.start();
+        printGroupInfo(appThread);
+    }
+
+    static void printGroupInfo(Thread t) {
+        ThreadGroup group = t.getThreadGroup();
+        System.out.println("thread " + t.getName() + " group name is "
+                + group.getName()+ " max priority is " + group.getMaxPriority()
+                + " thread count is " + group.activeCount());
+
+        ThreadGroup parent=group;
+        do {
+            ThreadGroup current = parent;
+            parent = parent.getParent();
+            if (parent == null) {
+                break;
+            }
+
+            System.out.println(current.getName() + "'s parent is " + parent.getName());
+        } while (true);
+        System.out.println("--------------------------");
+    }
 }
