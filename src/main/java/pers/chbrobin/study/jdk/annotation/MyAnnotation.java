0b29180f87e427901f855de466c6beac66f3c381
chenhuibin@superjia.com <chenhuibin>
2017-06-09 17:00:10 +0800

    project moduleble

--- src/main/java/pers/chbrobin/study/jdk/annotation/MyAnnotation.java
+++
@@ -1,7 +0,0 @@
-package pers.chbrobin.study.jdk.annotation;
-
-/**
- * Created by Administrator on 2017/6/7 0007.
- */
-public @interface MyAnnotation {
-}
--- src/main/java/pers/chbrobin/study/thread/ThreadLocalTest.java
+++
@@ -1,7 +0,0 @@
-package pers.chbrobin.study.thread;
-
-/**
- * Created by Administrator on 2017/6/5 0005.
- */
-public class ThreadLocalTest {
-}
