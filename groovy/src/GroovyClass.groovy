d9bf39de39dc97ce1af6aaaaeec876c44d907730
chenhuibin@superjia.com <chenhuibin>
2017-06-09 18:16:38 +0800

    groovy test

---
+++ groovy/src/GroovyClass.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/6/9 0009.
+ */
+class GroovyClass {
+    public test() {
+        println "test"
+    }
+    static void main(args) {
+        GroovyClass groovyTest = new GroovyClass();
+        groovyTest.test();
+    }
+}
---
+++ groovy/src/GroovyScript.groovy
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2017/6/9 0009.
+ */
+println "test"
