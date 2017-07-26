8dc1003e203d48de5499010ff4bb9ad6f5b17cc5
chenhuibin@superjia.com <chenhuibin>
2017-07-26 16:43:56 +0800

    groovy & scala script code

---
+++ groovy/src/HelloWorld.groovy
@@ -0,0 +1,4 @@
+/**
+ * Created by chenhuibin on 2017/7/26 0026.
+ */
+println("test");
---
+++ scala/src/HelloWorld.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by chenhuibin on 2017/7/26 0026.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!");
+    var test : String = "abc";
+    printf(test);
+  }
+}
